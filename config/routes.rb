c829485d1cd24ee20093fcfe635630b78494a9da
marxelo <marceloc007@hotmail.com>
2018-02-27 05:37:48 -0300

    se terminan las vistas de devise (login y registro)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
  
   get "articles/search"
-  devise_for :users
+  
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    passwords: "user/password",
+  }
   resources :categories
  # get 'articles/index'
    get 'welcome/index'
